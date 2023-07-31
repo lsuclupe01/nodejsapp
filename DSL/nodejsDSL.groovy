f633476c8005de9282058f810c11bf1821665ef3
Luis Suclupe <lsuclupe@gmail.com>
2023-07-31 11:52:07 -0500

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -10,7 +10,7 @@ job('Aplicacion Node.js DSL') {
         scm('H/7 * * * *')
     }
     wrappers {
-        nodejs('nodejs')
+        nodejs('nodeJs')
     }
     steps {
         shell("npm install")
